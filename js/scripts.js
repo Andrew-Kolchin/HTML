6055fd54971cd76182f13da5c10384ca22104bcb
Andrei Kolchin <masterrandrew@gmail.com>
2019-03-20 16:46:20 +0200

    fix responsive. add forms

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,19 @@
 
 var displayFormButton = document.getElementById('displayForm');
 var subscribeArea = document.getElementsByClassName('promo__subscribeArea')[0];
+var openBlock = document.getElementsByClassName('openBlock');
 
-displayFormButton.addEventListener('click', function (event) {
-  event.target.style.opacity = 0;
-  subscribeArea.style.opacity = 1;
-});
+if(displayFormButton){
+  displayFormButton.addEventListener('click', function (event) {
+    event.target.style.opacity = 0;
+    subscribeArea.style.opacity = 1;
+  });
+}
+
+for (var i = 0; i < openBlock.length; i++) {
+  openBlock[i].addEventListener('click', function (event) {
+    event.target.parentElement.nextElementSibling.style.display = "block";
+    event.target.parentElement.style.display = "none";
+  });
+}
+
